1e245fd2f8a31e942ef8626a4edf88ad0433ebdb
ndote <56348299+ndote@users.noreply.github.com>
2022-06-23 18:32:32 +1000

    secret key update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import psycopg2
 
 app = Flask(__name__, template_folder='templates')
 Bootstrap(app)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL_UPDATED', 'sqlite:///portfolio.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
