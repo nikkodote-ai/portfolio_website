9448b11629ca220d4dd1b99a6e0e6ca1e4cb83d6
ndote <56348299+ndote@users.noreply.github.com>
2022-06-27 04:17:05 +1000

    Updated output path to abs path

--- apps/audiotopdfconverter/text_converter_app.py
+++ apps/audiotopdfconverter/text_converter_app.py
@@ -109,11 +109,11 @@ def long_audio_download(output_uri):
     s3 = client('s3', region_name="ap-southeast-2", aws_access_key_id=AWSAccessKeyId, aws_secret_access_key=AWSSecretKey)
     print(bucket_name + "\n" + object_name)
     try:
-        s3.download_file(bucket_name, object_name, os.path.join("C:\\Users\\nikko\\Downloads\\", object_name))
+        s3.download_file(bucket_name, object_name, os.path.join("C:\\Users\\nikko\\Downloads", object_name))
     except ClientError as e:
         print("Processing. Download once done")
         time.sleep(20)
-        s3.download_file(bucket_name, object_name, os.path.join("C:\\Users\\nikko\\Downloads\\", object_name))
+        s3.download_file(bucket_name, object_name, os.path.join("C:\\Users\\nikko\\Downloads", object_name))
     else:
         raise
 
