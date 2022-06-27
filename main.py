0a8fdf3279ff488349645fa06766fb531512e18e
ndote <56348299+ndote@users.noreply.github.com>
2022-06-27 13:19:46 +1000

    minor type error

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def text_audio_converter():
         text_to_convert = tca.convert_ppt_to_text(file_location_pptx)
         file_object = tca.convert_to_audio(text_to_convert, form.voice.data, form.engine.data, file_location_mp3)
         return Response(
-            file_object['Body'].Read(),
+            file_object['Body'].read(),
             mimetype='text/plain',
             headers={'Content-Disposition': f"attachment;filename{generated_filename}.mp3"}
         )
