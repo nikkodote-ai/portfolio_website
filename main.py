afb0aa4c609f31a9686cf8a77a477bf9a059fcf8
ndote <56348299+ndote@users.noreply.github.com>
2022-06-29 22:26:11 +1000

    fix error

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ csrf = CSRFProtect(app)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL_UPDATED', 'sqlite:///portfolio.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('sqlite:///portfolio.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # for uploading files
